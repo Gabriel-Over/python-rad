4459040d22064a6c45418462835e5d44c687d93e
Gabriel Ryan <ryang6361@gmail.com>
2026-03-19 08:30:32 -0300

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,13 @@
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite outro numero: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print("-------------------------------")
+print(f"A soma de {num1} e {num2} é: {soma}")
+print(f"A subtração de {num1} e {num2} é: {subtracao}")
+print(f"A multiplicação de {num1} e {num2} é: {multiplicacao}")
+print(f"A divisão de {num1} e {num2} é: {divisao}")
