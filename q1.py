17cb964a271d11035c56e39923f95ea9158314d1
Gabriel Ryan <ryang6361@gmail.com>
2026-03-19 08:22:46 -0300

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura: ")
+
+print('------------------------------------')
+
+print(f'Seu nome é: {nome}')
+print(f'Sua idade é: {idade}')
+print(f'Sua altura é: {altura}')
